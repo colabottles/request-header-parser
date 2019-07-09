81fefc381acaeca39079676cdeb5f7545b8e371b
Glitch (torpid-darkness) <none>
2019-07-09 02:36:38 +0000

    🤘🍮 Checkpoint ./server.js:1692546/4769

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-var bodyParser = require('body-parser');
+var staticFiles = __dirname + '/public';
+var indexHTML = __dirname + '/views/index.html';
 
-app.get('/',(req,res)=>{
-    var lang = req.headers["accept-language"].split(",")[0];
-    var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
-    var out = {"ipaddress":req.ip,"language":lang,"software":software};
-   res.send(out); 
+app.get('/', function(req, res) {
+    res.sendFile(indexHTML);
 });
 
-app.get("/",(req,res)=>{
-   res.send(req.body); 
+app.get('/headers', function(req, res) {
+
+    var ip = req.headers["x-forwarded-for"].split(',').splice(0,1).toString();
+    var url = req.headers['host'];
+    var lang  = req.headers['accept-language'].split(',')[0]
+    var sys = req.headers['user-agent'].match(/\(([^)]+)\)/g)[0].slice(1,-1);
+
+    var obj = {
+        //"IP Address": ip,
+        "IP Address": url,
+        "Language" : lang,
+        "Operating System": sys
+    }
+
+    res.json(obj);
 });
 
-app.listen(port, function () {
-  console.log('App running on port '+port+"!");
+app.use(express.static(staticFiles));
+
+app.listen(port, function() {
+    console.log(`Server is listening on port : ${port}`)
 });
